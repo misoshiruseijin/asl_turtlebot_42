07d06a70abab01c9db2f5675e0a3678c245fb7be
misoshiruseijin <ayano.hiranaka@gmail.com>
2022-12-09 16:05:05 -0800

    gmapping working?

--- scripts/planners/path_smoother.py
+++ scripts/planners/path_smoother.py
@@ -22,8 +22,30 @@ def compute_smoothed_traj(path, V_des, k, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    raise NotImplementedError # REPLACE THIS FUNCTION WITH YOUR IMPLEMENTATION
+    # Hint 1 - Determine nominal time for each point in the path using V_des
+    # Hint 2 - Use splrep to determine cubic coefficients that best fit given path in x, y
+    # Hint 3 - Use splev to determine smoothed paths. The "der" argument may be useful.
+    import sys
+    np.set_printoptions(threshold=sys.maxsize)
+    path = np.array(path)
+    N = path.shape[0]
+    t = np.zeros(N)
+    t[1:] = np.cumsum(np.linalg.norm(path[:-1] - path[1:], axis=1) / V_des)
+    t_smoothed = np.arange(0,t[-1]+dt, dt)
+    N = t_smoothed.shape[0]
+
+    tck_x = scipy.interpolate.splrep(x=t, y=path[:,0], k=k, s=alpha)
+    tck_y = scipy.interpolate.splrep(x=t, y=path[:,1], k=k, s=alpha)
+    
+    x_d = scipy.interpolate.splev(t_smoothed, tck_x, der=0)
+    y_d = scipy.interpolate.splev(t_smoothed, tck_y, der=0)
+    xd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=1)
+    yd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=1)
+    xdd_d = scipy.interpolate.splev(t_smoothed, tck_x, der=2)
+    ydd_d = scipy.interpolate.splev(t_smoothed, tck_y, der=2)
+    theta_d = np.arctan2(yd_d, xd_d)
     ########## Code ends here ##########
     traj_smoothed = np.stack([x_d, y_d, theta_d, xd_d, yd_d, xdd_d, ydd_d]).transpose()
 
     return t_smoothed, traj_smoothed
+
